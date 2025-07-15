7a10ee5569ce8e7e146ab141104e20aa03efa3ec
Isaac-Monted <isaacmonted10@gmail.com>
2025-07-15 07:37:07 -0600

    Actualiza el contenido y estilo de la invitación digital, incluyendo nuevos textos y estilos responsivos para la tarjeta de invitación. Queda concluida la seccion del boleto dijital y se inicia con la animacion de los novios caminantes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// constantes
+const Fecha_boda = '8 de Noviembre';
+
 // Escucha el evento de carga del DOM
 document.addEventListener("DOMContentLoaded", () => {
     colocarHistoria1();
@@ -16,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function colocarHistoria1() {
-    const $Historia = `Fue un 26 de diciembre cuando dos desconocidos de encontrarian el uno al otro sin pensar que llegarían a ser tan importantes el uno para el otro. Fue amor a primera vista, ya que cada que ellos dos estaban juntos hacian del lugar algo unico, sin importar donde o cuando, su compañia era mas que suficiente.<br>`;
+    const $Historia = `La historia que te voy a contar es muy especial, ya que se trata de un amor que empezo con una coincidencia o un accidente que conectarioa a dos personas que habian perdido la esperanza en el amor. pero todo esto cambiaria un 26 de diciembre cuando dos desconocidos se encontrarian el uno al otro sin pensar que llegarían a ser tan importantes el uno para el otro. Fue amor a primera vista, cada vez que ellos dos estaban juntos hacian del lugar algo unico, sin importar donde o cuando, su compañia era mas que suficiente.<br> No paso mucho tiempo cuando un 28 de enero decidirian por fin estar juntos<br>`;
 
     const contenedor = document.getElementById("Historia"); // Selecciona el contenedor
     const elementoP = document.createElement("p"); // Crea un elemento <p>
@@ -25,7 +28,7 @@ function colocarHistoria1() {
 };
 
 function colocarHistoria2() {
-    const $Historia = `No paso mucho tiempo cuando un 28 de enero decidirian por fin estar juntos<br> iwefouhwefewf`;
+    const $Historia = `Y desde hay empezaria a escribirse un sin fin de historias y momentos inolvidables, un 26 de enero tomarian la decision de formalizar el compromiso pasando se ser novios a ser marido y mujer. Han pasado por muchos obstaculos y aunque no ha sido facil llegar a este punto, hay algo de lo que si estoy seguro, su amor es increbrantable y realmente quieren permanecer juntos por el resto de sus vidas. <br> desafortunadamente no te podre contar que es lo que sigue en esta historia ya que aun se esta escribiendo, pero si te puedes ser participe de ella este ${Fecha_boda} que sera el comienzo de un nuevo capitulo para estas dos personitas que se juraran amor eterno hasta que la muerte los separe.`;
 
     const contenedor = document.getElementById("Historia2"); // Selecciona el contenedor
     const elementoP = document.createElement("p"); // Crea un elemento <p>
