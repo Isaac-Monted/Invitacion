4012573e83be8503d48692947d44d1fe74530c28
Isaac-Monted <isaacmonted10@gmail.com>
2025-07-22 13:43:49 -0600

    se agrega sonido a la pagina, se transfroma el contenedor de las luciernagas en un boton de play y pausa para la musica, se corrigen errores ortograficos en la historia, se corrigen estilos y tamaños en ventanas chicas, se reutiliza la seccion de vestimenta para la seccion de los padres

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
         contadorDiv.id = 'contador';
         document.getElementById('Contenedor_corazon').appendChild(contadorDiv);
     }
+
+    // Colocar la musica de fondo
+    reproducirAudioAlScroll();
     
     actualizarContador(); // Función para actualizar el contador
     setInterval(actualizarContador, 1000);
@@ -34,8 +37,28 @@ document.addEventListener("DOMContentLoaded", () => {
     animations.AnimacionCodigoVestimenta();
 });
 
+function reproducirAudioAlScroll() {
+    const contenedor = document.getElementById('contenedor-luciernagas');
+    const audio = document.getElementById('CancionFondo');
+
+    contenedor.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play()
+                .then(() => {
+                    console.log("🎵 Audio reproducido");
+                })
+                .catch(err => {
+                    console.warn("❌ El navegador bloqueó la reproducción:", err);
+                });
+        } else {
+            audio.pause();
+            console.log("⏸️ Audio pausado");
+        }
+    });
+}
+
 function colocarHistoria1() {
-    const $Historia = `La historia que te voy a contar es muy especial, ya que se trata de un amor que empezo con una coincidencia o un accidente que conectarioa a dos personas que habian perdido la esperanza en el amor. pero todo esto cambiaria un 26 de diciembre cuando dos desconocidos se encontrarian el uno al otro sin pensar que llegarían a ser tan importantes el uno para el otro. Fue amor a primera vista, cada vez que ellos dos estaban juntos hacian del lugar algo unico, sin importar donde o cuando, su compañia era mas que suficiente.<br> No paso mucho tiempo cuando un 28 de enero decidirian por fin estar juntos<br>`;
+    const $Historia = `La historia que te voy a contar es muy especial, ya que se trata de un amor que empezó gracias a una coincidencia o un accidente que conectaría a dos personas que habían perdido la esperanza en el amor. pero todo esto cambiaria un 26 de diciembre cuando dos desconocidos se encontrarían el uno al otro sin pensar que llegarían a ser tan importantes el uno para el otro. Fue amor a primera vista, cada vez que ellos dos estaban juntos hacían del lugar algo único, sin importar donde o cuando, su compañía era más que suficiente.<br> No paso mucho tiempo cuando un 28 de enero decidirían por fin estar juntos<br>`;
 
     const contenedor = document.getElementById("Historia"); // Selecciona el contenedor
     const elementoP = document.createElement("p"); // Crea un elemento <p>
@@ -44,7 +67,7 @@ function colocarHistoria1() {
 };
 
 function colocarHistoria2() {
-    const $Historia = `Y desde hay empezaria a escribirse un sin fin de historias y momentos inolvidables, un 26 de enero tomarian la decision de formalizar el compromiso pasando se ser novios a ser marido y mujer. Han pasado por muchos obstaculos y aunque no ha sido facil llegar a este punto, hay algo de lo que si estoy seguro, su amor es increbrantable y realmente quieren permanecer juntos por el resto de sus vidas. <br> desafortunadamente no te podre contar que es lo que sigue en esta historia ya que aun se esta escribiendo, pero si te puedes ser participe de ella este ${Fecha_boda} que sera el comienzo de un nuevo capitulo para estas dos personitas que se juraran amor eterno hasta que la muerte los separe.`;
+    const $Historia = `Y desde hay empezaría a escribirse un sin fin de historias y momentos inolvidables, un 26 de enero tomarían la decisión de formalizar el compromiso pasando se ser novios a ser marido y mujer. Han pasado por muchos obstáculos y aunque no ha sido fácil llegar a este punto, hay algo de lo que si estoy seguro, su amor es inquebrantable y realmente quieren permanecer juntos por el resto de sus vidas. <br> desafortunadamente no te poder contar que es lo que sigue en esta historia ya que aún se está escribiendo, pero si te puedes ser partícipe de ella este ${Fecha_boda} que será el comienzo de un nuevo capítulo para estas dos personitas que se juraran amor eterno hasta que la muerte los separe.`;
 
     const contenedor = document.getElementById("Historia2"); // Selecciona el contenedor
     const elementoP = document.createElement("p"); // Crea un elemento <p>
